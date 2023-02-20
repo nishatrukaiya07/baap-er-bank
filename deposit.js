eebae9dbd3c6150be1b011c786688f63f579f10c
nishatrukaiya07 <nishatrukaiya07@gmail.com>
2023-02-21 01:01:59 +0600

    80% finished

--- deposit.js
+++ deposit.js
@@ -3,11 +3,29 @@ document.getElementById('btn-deposit').addEventListener('click', function(){
 // step-2:get the deposit amount from the deposit field
 // for input field use .value to the value inside the input field
 const depositField = document.getElementById('deposit-field');
-const depositAmount = depositField.value;
+const newDepositAmountString = depositField.value;
+const newDepositAmount = parseFloat(newDepositAmountString);
+
 // console.log(depositAmount);
-// step:get the current deposit total
+// step:3-get the current deposit total
 // for non-input (element other than input ,textarea) use innerText to get the text
 const depositTotalElement = document.getElementById('deposit-total');
-const depositTotal = depositTotalElement.innerText;
-depositTotalElement.innerText = depositAmount;
+const previousDepositTotalString = depositTotalElement.innerText;
+const previousDepositTotal= parseFloat(previousDepositTotalString);
+// step-4:add numbers to set the total deposit
+const currentDepositTotal = previousDepositTotal + newDepositAmount;
+// set the deposit total
+depositTotalElement.innerText = currentDepositTotal;
+// step-5:grt balance current total
+const balanceTotalElement = document.getElementById('balance-total');
+const previousBalanceTotalString= balanceTotalElement.innerText;
+const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+// step-6:
+const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+// set the balance element
+balanceTotalElement.innerText= currentBalanceTotal;
+
+
+// step-7:clear the deposit field
+depositField.value = '';
 })
---
+++ withdraw.js
@@ -0,0 +1,9 @@
+// step-1:
+document.getElementById('btn-withdraw').addEventListener('click', function(){
+    // step-2:
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount= parseFloat(newWithdrawAmountString);
+    // step-4:
+    
+})
